795244218c2b2628e28a4023c814723a06f3d9bd
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2021-02-17 12:09:50 +0300

    fix issue reg. invalid chars
    Signed-off-by: Ahmet Turkmen <f.ahmet.turkmen@icloud.com>

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ columns = ['Albümin/Kreatinin (Spot idrar)', 'Demir (Serum/Plazma)',
 		   'Kreatinin (Kreatinin (Serum/Plazma))', 'Kreatinin (Spot idrar)',
 		   'UIBC']
 
+def clean_invalid_chars(s):
+	try:
+		s = s.replace(',', '.')
+		i = float(s)
+		return i
+	except ValueError as verr:
+		return 0
+	except Exception as ex:
+		return 0
+
 if __name__ == '__main__':
 	files_to_read = []
 	data_frames = []
@@ -29,13 +39,11 @@ if __name__ == '__main__':
 		user_data = pd.read_excel(d)
 		df = user_data[['Test Adı', 'Sonuç']]
 		df = df.fillna(0)
-		df = df[pd.to_numeric(df['Sonuç'],
-							  errors='coerce').notnull()]  # some values are lost due to some invalid characters
+		df['Sonuç'] = df['Sonuç'].apply(lambda x: clean(x))
 		df['Sonuç'] = df['Sonuç'].astype(float)
 		df_mean = df.groupby('Test Adı', as_index=True).mean()
 		d = df_mean.T
 		user_value = d[d.columns.intersection(columns)]
-
 		f = user_value.rename(columns={'Test Adı': 'Isim Soyisim'}, index={'Sonuç': filename.split('.xlsx')[0]})
 		data_frames.append(f)
 
